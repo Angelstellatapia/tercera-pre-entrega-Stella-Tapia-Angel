1fb9136477e28f5c3ec9f795f40ef2f304913431
angel leonel stella tapia <leonelstella@hotmail.com>
2023-05-29 22:26:22 -0300

    correccion de index

--- project/config/urls.py
+++ project/config/urls.py
@@ -20,8 +20,8 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(("home.urls", "home"))),
-    path("cliente/", include(("cliente.urls", "cliente"))),
-    path("productos/", include(("productos.urls", "productos"))),
-    path("ventas/", include(("ventas.urls", "ventas"))),
+    path("cliente/", include(("cliente.urls",'cliente'))),
+    path("productos/", include(("productos.urls",'productos'))),
+    path("ventas/", include(("ventas.urls",'ventas'))),
    
 ]
